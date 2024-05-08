f238cb795700307b90c4b1f29aac771d04a87a17
JosseGonnza <accion.quevedo@hotmail.com>
2024-05-08 09:50:53 +0100

    Spring Initilizr

---
+++ src/main/java/com/jossegonnza/aplicationrest/AplicationRestApplication.java
@@ -0,0 +1,13 @@
+package com.jossegonnza.aplicationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jossegonnza/aplicationrest/AplicationRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jossegonnza.aplicationrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
